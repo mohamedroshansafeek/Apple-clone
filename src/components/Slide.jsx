19e2118d8f861a7ce386441823dd33794238df5d
mohamedroshansafeek <mohamedroshansafeeky@gmail.com>
2025-08-04 12:33:11 +0530

    updated slide file

--- src/components/Slide.jsx
+++ src/components/Slide.jsx
@@ -1,22 +1,20 @@
 import React, { useState, useEffect } from "react";
+import img1 from "../assets/images/movie-01.png";
+import img2 from "../assets/images/movie-02.png";
+import img3 from "../assets/images/movie-03.png";
+import img4 from "../assets/images/movie-04.png";
+import img5 from "../assets/images/movie-05.png";
+import img6 from "../assets/images/movie-06.png";
+import img7 from "../assets/images/movie-07.png";
+import img8 from "../assets/images/movie-08.png";
+import img9 from "../assets/images/movie-09.png";
+import img10 from "../assets/images/movie-10.png";
 
-const images = [
-  "/src/assets/images/movie-01.png",
-  "/src/assets/images/movie-02.png",
-  "/src/assets/images/movie-03.png",
-  "/src/assets/images/movie-04.png",
-  "/src/assets/images/movie-05.png",
-  "/src/assets/images/movie-06.png",
-  "/src/assets/images/movie-07.png",
-  "/src/assets/images/movie-08.png",
-  "/src/assets/images/movie-09.png",
-  "/src/assets/images/movie-10.png",
-];
+const images = [img1, img2, img3, img4, img5, img6, img7, img8, img9, img10];
 
 const Slideshow = () => {
   const [currentIndex, setCurrentIndex] = useState(0);
 
-  
   useEffect(() => {
     const interval = setInterval(() => {
       setCurrentIndex((prev) => (prev + 1) % images.length);
@@ -24,9 +22,8 @@ const Slideshow = () => {
     return () => clearInterval(interval);
   }, []);
 
-
   return (
-    <div className="relative w-full h-64 sm:h-80 md:h-screen overflow-hidden mt-5">
+    <div className="relative w-full h-screen overflow-hidden mt-5">
       {images.map((img, index) => (
         <img
           key={index}
@@ -38,10 +35,7 @@ const Slideshow = () => {
         />
       ))}
     </div>
-   
-
   );
 };
 
 export default Slideshow;
-
